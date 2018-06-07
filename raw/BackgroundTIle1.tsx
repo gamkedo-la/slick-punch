6aefc9c25bb42e19e8442d50f11c037ba02fe18f
techieVignesh <r.vignesh149@gmail.com>
2018-06-07 17:56:25 +0530

    New tileset

---
+++ raw/BackgroundTIle1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="BackgroundTIle1" tilewidth="35" tileheight="35" tilecount="1" columns="1">
+ <image source="E:/Contributions/gaming/Gamkedo/slickPunch/raw/Sprite-0001.png" width="35" height="35"/>
+</tileset>
---
+++ raw/slickTileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="slickTileset2" tilewidth="34" tileheight="35" tilecount="6" columns="3">
+ <image source="E:/Contributions/gaming/Gamkedo/slickPunch/raw/slickTileset.png" trans="821e1e" width="105" height="70"/>
+</tileset>
