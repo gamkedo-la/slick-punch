9e4ccbba4f894fc70e3f8185e101e58e28163485
techieVignesh <r.vignesh149@gmail.com>
2018-06-10 13:28:36 +0530

    Spritesheets generated for multiple movements

---
+++ raw/SLickPunch.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="SLickPunch" tilewidth="35" tileheight="35" tilecount="6" columns="3">
+ <image source="E:/Contributions/gaming/Gamkedo/slickPunch/raw/slickTileset.png" width="105" height="70"/>
+</tileset>
