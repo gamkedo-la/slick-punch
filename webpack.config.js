dfff205a86ae1186e34992c0336b5471ba5e8858
techieVignesh <r.vignesh149@gmail.com>
2018-07-24 14:48:05 +0530

    Webpack config file, babelrc, eslint file added

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  module: {
+    rules: [
+        {
+          test: /\.js$/i,
+          enforce: "pre",
+          exclude: /node_modules/,
+          use: [
+            	  { loader: "eslint-loader",
+            	    options: {
+            	             failOnError: false,
+            	             failOnWarning: false,
+                           fix: false
+            	    },
+            	  },
+            ]
+        },
+
+        {
+          test: /\.js$/i,
+          enforce: "pre",
+          exclude: /node_modules/,
+          use: [
+                { loader: "babel-loader"},
+            ]
+        },
+
+      	
+        {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader'
+            ]
+        },
+
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
+        }
+     ]
+  },
+  
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
