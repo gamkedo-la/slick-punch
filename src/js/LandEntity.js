5a95dd31d9d484c7c8024b3ab2b10738459992ff
techieVignesh <r.vignesh149@gmail.com>
2018-08-05 14:03:17 +0530

    All player functions converted into prototype methods

--- src/js/LandEntity.js
+++ src/js/LandEntity.js
@@ -10,9 +10,16 @@ const PLAYER_COLLISION_PADDING = 5;
 //Has collision code for platform 
 //Had properties that help check player Collision
 const FLYING_ENEMY_HEALTH = 1;
-const PLAYER_HEALTH = 3;
-const DUMB_WALK_ENEMY = 1;
+const FLYING_ENEMY_ATTACK_POWER = 2;
+
 const RUNNING_ZOMBIE_ENEMY = 1; 
+const RUNNING_ZOMBIE_ATTACK_POWER = 1.5; 
+
+const PLAYER_ATTACK_POWER = 1.5;
+const PLAYER_HEALTH = 3;
+
+const ENEMY_DUMB_ATTACK_POWER = 1;
+const ENEMY_DUMB_HEALTH = 1;
 
 //constants created instead of just texts to make sure no one 
 //mispells and assigns different state
@@ -94,13 +101,14 @@ entityClass.prototype.setStateValueTo = function (key, val){
 entityClass.prototype.takeDamage = function (howMuch) {
 	console.log("Damage received:  " + howMuch + " by " + this.name);
 	//TODO: Improve this. Currently only effects health till isHurt is active. 
-	if(this.health > 0 && !this.state.isHurt){
-		this.health -= howMuch;
-		this.state.isHurt = true;
-	}
+	if (this.health > 0 && !this.state[HURT]) {
+    this.health -= howMuch;
+    this.state[HURT] = true;
+    playerHitEffect(this.pos.x, this.pos.y);
+  }
 	if (this.health <= 0) {
 		console.log("PLAYER HAS 0 HP - todo: gameover/respawn");
-		this.state.isDead = true;
+    this.state[DEAD] = true;
 		if(	this.name == "Player"){
 			setTimeout(this.resetDeadAnimation.bind(this), 500);
 		}
@@ -108,69 +116,82 @@ entityClass.prototype.takeDamage = function (howMuch) {
 	this.resetHurtTimeout = setTimeout(this.resetHurtAnimation.bind(this), 700);
 }
 
-	this.resetHurtAnimation = function(){
-		this.state.isHurt = false;
+entityClass.prototype.resetHurtAnimation = function () {
+  this.state[HURT] = false;
+}
+
+entityClass.prototype.init = function (whichImage, playerName) {
+  this.name = playerName;
+  this.playerPic = whichImage;
+  this.doubleJumpCount = 0;
+  this.state = {
+    'isOnGround': true,
+    'isIdle': true,
+    'isInMotion': false,
+    'isMovingLeft': false, //Required to set character flip.
+    'isCrouching': false,
+    'isFacingUp': false, //Might be redundant
+    'isAttacking': false, //combo punches, kick on 3 continuos punch
+    'isDefending': false,
+    'isAnimating': false, // Used to set state between animation and final.
+    'isHurt': false,
+    'isDead': false,
+    'isFlying': false
+  };
+  var entityToWorldValue;
+  switch(playerName){
+    case "Player":
+      entityToWorldValue = WORLD_PLAYERSTART;
+      this.attackPower = PLAYER_ATTACK_POWER;
+      this.health = PLAYER_HEALTH;
+      break;
+    case "Dumb Enemy":
+      entityToWorldValue = WORLD_ENEMY_DUMB_START;
+      this.attackPower = ENEMY_DUMB_ATTACK_POWER;
+      this.health = ENEMY_DUMB_HEALTH;
+      break;
+
+  }
+  this.setEntityPosition(entityToWorldValue);
+} // end of playerReset func
+
+
+
+function addEntityToWorld(){
+	for (var eachRow = 0; eachRow < WORLD_ROWS; eachRow++) {
+		for (var eachCol = 0; eachCol < WORLD_COLS; eachCol++) {
+			var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
+			if (worldGrid[arrayIndex] == this.valueInWorldIndex) {
+				worldGrid[arrayIndex] = WORLD_BACKGROUND;
+				// this.ang = -Math.PI/2;
+				this.pos.x = eachCol * WORLD_W + WORLD_W / 2;
+				this.pos.y = eachRow * WORLD_H + WORLD_H / 2;
+				return;
+			} // end of player start if
+		} // end of col for
+	} // end of row for
+	console.log("NO" + this.name + "START FOUND!");
+}
+
+this.move = function () {
+	this.boundingBox.width = this.width/3;
+	this.boundingBox.height = this.height;
+	this.boundingBox.x = this.pos.x - this.boundingBox.width/2;
+	this.boundingBox.y = this.pos.y - this.boundingBox.height/2;
+} // end of player.move function
+
+this.draw = function () {
+	//TODO : Each animation should take atmost 1 sec to complete. 
+	//TODO : Clean all code. 
+	//TODO : Jump, Attack animation should work Only once
+	if (this.state['isIdle']) {
+		this.spriteAnim = this.idleAnim;
+		// this.framesAnim = this.spriteAnim.frameNum;
 	}
-
-	this.resetDeadAnimation = function(){
-		//Reset level data as well. 
-		loadLevel(levelOne)
+	if (this.spriteAnim !=null) {			
+		//TODO :Change this.frameRow and used it for animating consilated spritesheet of player character
+		this.spriteAnim.draw(this.spriteAnim.frameIndex, this.frameRow, this.pos.x, this.pos.y, this.ang, this.state.isMovingLeft);
+		// console.log(this.spriteAnim.frameIndex);
 	}
-
-	this.reset = function () {
-		this.health = health;
-		this.state = {
-			'isOnGround': true,
-			'isIdle': true,
-			'isInMotion': false,
-			'isMovingLeft': false, //Required to set character flip.
-			'isCrouching': false,
-			'isFacingUp': false, //Might be redundant
-			'isAttacking': false, //combo punches, kick on 3 continuos punch
-			'isDefending': false,
-			'isAnimating' : false, // Used to set state between animation and final.
-			'isHurt': false,
-			'isDead': false
-		};
-		addEntityToWorld();
-	} // end of playerReset func
-
-
-	function addEntityToWorld(){
-		for (var eachRow = 0; eachRow < WORLD_ROWS; eachRow++) {
-			for (var eachCol = 0; eachCol < WORLD_COLS; eachCol++) {
-				var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
-				if (worldGrid[arrayIndex] == this.valueInWorldIndex) {
-					worldGrid[arrayIndex] = WORLD_BACKGROUND;
-					// this.ang = -Math.PI/2;
-					this.pos.x = eachCol * WORLD_W + WORLD_W / 2;
-					this.pos.y = eachRow * WORLD_H + WORLD_H / 2;
-					return;
-				} // end of player start if
-			} // end of col for
-		} // end of row for
-		console.log("NO" + this.name + "START FOUND!");
-	}
-
-	this.move = function () {
-		this.boundingBox.width = this.width/3;
-		this.boundingBox.height = this.height;
-		this.boundingBox.x = this.pos.x - this.boundingBox.width/2;
-		this.boundingBox.y = this.pos.y - this.boundingBox.height/2;
-	} // end of player.move function
-
-	this.draw = function () {
-		//TODO : Each animation should take atmost 1 sec to complete. 
-		//TODO : Clean all code. 
-		//TODO : Jump, Attack animation should work Only once
-		if (this.state['isIdle']) {
-			this.spriteAnim = this.idleAnim;
-			// this.framesAnim = this.spriteAnim.frameNum;
-		}
-		if (this.spriteAnim !=null) {			
-			//TODO :Change this.frameRow and used it for animating consilated spritesheet of player character
-			this.spriteAnim.draw(this.spriteAnim.frameIndex, this.frameRow, this.pos.x, this.pos.y, this.ang, this.state.isMovingLeft);
-			// console.log(this.spriteAnim.frameIndex);
-		}
-	}
+}
 
--- src/js/player.js
+++ src/js/player.js
@@ -120,294 +120,291 @@ function playerClass() {
 	//Used for animation.
 	//TODO :Change this.frameRow and used it for animating consilated spritesheet of player character
 	this.frameRow = 0;
-
-	//Used to track sound play and pause.
-	this.justPunched = false;
-	this.justJumped = false;
-	this.justKicked = false;
 	this.doubleJumpCount = 0;
 
-	//sets all values of state object to false
-	this.setStateToFalse = function () {
-		for (key in this.state) {
-			this.state[key] = false;
-		}
-	};
-
-	//sets value of given key pf state object to value passed
-	this.setStateValueTo = function (key, val) {
-		if (this.state.hasOwnProperty(key)) {
-			// console.log("Setting" + key + ":" + val);
-			this.state[key] = val;
-		}
-	};
-
-	this.setupInput = function (upKey, rightKey, downKey, leftKey, attackKey, jumpKey, defendKey) {
-		this.controlKeyUp = upKey;
-		this.controlKeyRight = rightKey;
-		this.controlKeyDown = downKey;
-		this.controlKeyLeft = leftKey;
-		this.controlKeyAttack = attackKey;
-		this.controlKeyJump = jumpKey;
-		this.controlKeyDefend = defendKey;
-	}
-
-	this.takeDamage = function (howMuch) {
-		console.log("Damage received: " + howMuch);
-		if (this.health > 0 && !this.state.isHurt) {
-			this.health -= howMuch;
-			this.state.isHurt = true;
-			playerHitEffect(this.pos.x, this.pos.y);
-		}
-		if (this.health <= 0) {
-			console.log("PLAYER HAS 0 HP - todo: gameover/respawn");
-			this.state.isDead = true;
-			playerDeathEffect(this.pos.x, this.pos.y);
-			setTimeout(this.resetGame.bind(this), 800);
-		}
-		this.resetHurtTimeout = setTimeout(this.resetHurtAnimation.bind(this), 500);
-	}
-
-	this.resetHurtAnimation = function () {
-		this.state.isHurt = false;
-	}
-
-	this.resetGame = function () {
-		loadLevel(levelOne)
-	}
-
-	this.init = function (whichImage, playerName) {
-		this.name = playerName;
-		this.playerPic = whichImage;
-		this.health = PLAYER_HEALTH;
-		this.doubleJumpCount = 0;
-		this.attackPower = PLAYER_ATTACK_POWER;
-		this.state = {
-			'isOnGround': true,
-			'isIdle': true,
-			'isInMotion': false,
-			'isMovingLeft': false, //Required to set character flip.
-			'isCrouching': false,
-			'isFacingUp': false, //Might be redundant
-			'isAttacking': false, //combo punches, kick on 3 continuos punch
-			'isDefending': false,
-			'isAnimating': false, // Used to set state between animation and final.
-			'isHurt': false,
-			'isDead': false,
-			'isFlying': false
-		};
-		this.setEntityPosition(WORLD_PLAYERSTART);
-	} // end of playerReset func
-
-	window.player = this;
-
-	this.setEntityPosition = function(entityWorldIndex){
-		for (var eachRow = 0; eachRow < WORLD_ROWS; eachRow++) {
-			for (var eachCol = 0; eachCol < WORLD_COLS; eachCol++) {
-				var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
-				if (worldGrid[arrayIndex] == entityWorldIndex) {
-					worldGrid[arrayIndex] = WORLD_BACKGROUND;
-					// this.ang = -Math.PI/2;
-					this.pos.x = eachCol * WORLD_W + WORLD_W / 2;
-					this.pos.y = eachRow * WORLD_H + WORLD_H / 2;
-					return;
-				} // end of player start if
-			} // end of col for
-		} // end of row for
-		console.log("NO" + this.name + "START FOUND!");
-	}
-
-	this.move = function () {
-		this.boundingBox.width = this.width / 3;
-		this.boundingBox.height = this.height;
-		this.boundingBox.x = this.pos.x - this.boundingBox.width / 2;
-		this.boundingBox.y = this.pos.y - this.boundingBox.height / 2;
-
-		if (this.state[ON_GROUND]) {
-			// this.speed.x *= GROUND_FRICTION;
-			this.speed.setX(this.speed.x * GROUND_FRICTION);
-			this.doubleJumpCount = 0;
-		}
-		else { 
-			// in the air
-			this.speed.setX(this.speed.x * AIR_RESISTANCE);
-			this.speed.setY(this.speed.y + GRAVITY);
-			// improve this
-			if (this.speed.y > this.boundingBox.height / 2 + PLAYER_COLLISION_PADDING) {
-				this.speed.y = this.boundingBox.height / 2 + PLAYER_COLLISION_PADDING;
-			}
-		}
-		//Left movement
-		if (this.keyHeld_Left) {
-			//this.setStateToFalse(); //setting every value of object to false; // might be buggy
-			this.setStateValueTo(IN_MOTION, true);
-			this.setStateValueTo(MOVING_LEFT, true);
-			this.speed.setX(-PLAYER_RUN_SPEED);
-		}
-		//Right movement
-		else if (this.keyHeld_Right) {
-			//this.setStateToFalse();
-			this.setStateValueTo(IN_MOTION, true);
-			this.setStateValueTo(MOVING_LEFT, false);
-			this.speed.setX(PLAYER_RUN_SPEED);
-			// this.speed.x = PLAYER_RUN_SPEED;
-		}
-		else {
-			this.setStateValueTo(IN_MOTION, false);
-			this.setStateValueTo(IDLE, true);
-		}
-		//For crouched movement 
-		if (this.keyHeld_Down) {
-			//Up for uppercut
-			this.setStateValueTo(IDLE, false);
-			this.setStateValueTo(CROUCHING, true);
-			this.boundingBox.height = this.height / 2;
-			this.boundingBox.y = this.pos.y - this.boundingBox.height / 2;
-		}
-		else {
-			//Down for spin kick
-			this.setStateValueTo(CROUCHING, false);
-			this.setStateValueTo(IDLE, true);
-		}
-
-		if (this.keyHeld_Attack) {
-			this.setStateValueTo(IDLE, false);
-			this.setStateValueTo(IN_MOTION, false);
-			this.setStateValueTo(ATTACKING, true);
-			// if(utils.distance(player.pos,this.pos) < 60){
-			//           player.takeDamage(1);
-			//       }
-			punchSound.play();
-			this.boundingBox.width = this.width / 1.5;
-			this.boundingBox.x = this.pos.x - this.boundingBox.width / 2;
-
-			if (this.keyHeld_Up && this.state.isCrouching) {
-				this.speed.y -= 0.05;
-				this.state.isOnGround = false;
-			}
-		}
-		else {
-			this.setStateValueTo(ATTACKING, false);
-		}
-
-		if (this.keyHeld_Jump && !this.keyHeld_Up_lastframe) {
-			// this.setStateToFalse();
-			this.keyHeld_Up_lastframe = true;
-			if (this.state['isOnGround']) { // regular jump
-				jumpSound.play();
-				this.speed.setY( this.speed.getY() - JUMP_POWER);
-				this.setStateValueTo(ON_GROUND, false);
-			}
-			else if (this.doubleJumpCount < MAX_AIR_JUMPS) { // in the air?
-				jumpSound.play();
-				this.speed.setY(0);
-				this.speed.setY( this.speed.getY() - JUMP_POWER);
-				this.doubleJumpCount++;
-				this.setStateValueTo(ON_GROUND, false);
-			} 
-			else {
-				console.log("Ignoring triple jump...");
-			}
-		}
-		// avoid multiple jumps from the same keypress
-		this.keyHeld_Up_lastframe = this.keyHeld_Jump;
-
-		//Checking if player is falling or jumping.
-		//Get bounding box border coordinates and perform the same functionality for each point
-		if (this.speed.y < 0 && 
-			(isPlatformAtPixelCoord(this.pos.x + this.boundingBox.width/2, this.pos.y - this.boundingBox.height / 2) ||
-			isPlatformAtPixelCoord(this.pos.x - this.boundingBox.width/2, this.pos.y - this.boundingBox.height / 2))) {
-				this.pos.setY((Math.floor(this.pos.y / WORLD_H)) * WORLD_H + this.boundingBox.height / 2);
-				this.speed.setY(0);
-		}
-
-		if (this.speed.y > 0 && isPlatformAtPixelCoord(this.pos.x, this.pos.y + this.boundingBox.height / 2 - PLAYER_COLLISION_PADDING * 2)) {
-			this.pos.setY((1 + Math.floor(this.pos.y / WORLD_H)) * WORLD_H - this.boundingBox.height / 2 + PLAYER_COLLISION_PADDING);	
-			this.setStateValueTo(ON_GROUND, true);
-			this.speed.setY(0);
-		}
-		//checks for air/empty space
-		else if (!isPlatformAtPixelCoord(this.pos.x, this.pos.y + this.height / 2 + PLAYER_COLLISION_PADDING * 2)) {
-			this.setStateValueTo(ON_GROUND, false);
-		}
-		//Sideways collision
-		if (this.speed.x < 0 && 
-			(isPlatformAtPixelCoord(this.pos.x - this.boundingBox.width / 2, this.pos.y + this.boundingBox.height/4) ||
-			isPlatformAtPixelCoord(this.pos.x - this.boundingBox.width / 2, this.pos.y - this.boundingBox.height/4))){
-			this.pos.x = (Math.floor(this.pos.x / WORLD_W)) * WORLD_W + this.boundingBox.width / 2;
-		}
-
-		if (this.speed.x > 0 && 
-			(isPlatformAtPixelCoord(this.pos.x + this.boundingBox.width / 2, this.pos.y + this.boundingBox.height/4) ||
-			 isPlatformAtPixelCoord(this.pos.x + this.boundingBox.width / 2, this.pos.y - this.boundingBox.height/4))) {
-			this.pos.x = (1 + Math.floor(this.pos.x / WORLD_W)) * WORLD_W - this.boundingBox.width / 2;
-		}
-
-		this.pos.addTo(this.speed) // same as above, but for vertical
-		playerWorldHandling(this);
-
-		if (this.spriteAnim != null) {
-			this.spriteAnim.update();
-		}
-	}
-
-	this.draw = function () { 
-		//TODO : Jump, Attack animation should work Only once
-		//TODO : Reset punch animation loop 
-		if (this.state[IDLE]) {
-			this.spriteAnim = this.idleAnim;
-			// this.framesAnim = this.spriteAnim.frameNum;
-		}
-
-		if (this.state[IN_MOTION]) {
-			this.spriteAnim = this.walkAnim;
-		}
-
-		if (this.state[HURT]) {
-			this.spriteAnim = this.hurtAnim;
-		}
-
-		if (this.state[DEAD]) {
-			this.spriteAnim = this.deadAnim;
-		}
-		//Crouch Animation
-		if (this.state[CROUCHING]) {
-			this.spriteAnim = this.crouchAnim;
-			if (this.state.isInMotion) {
-				this.spriteAnim = this.rollAnim;
-			}
-		}
-		// if (this.state['isInMotion'] && this.state['isCrouching']) {
-		// }
-		if (this.state[ATTACKING]) {
-			this.spriteAnim = this.punchAnim;
-			if (this.state.isCrouching) {
-				if (this.keyHeld_Up) {
-					this.spriteAnim = this.uppercutAnim;
-				}
-				else {
-					this.spriteAnim = this.crouchedKickAnim;
-				}
-			}
-		}
-		//is Jumping or falling
-		if (!this.state[ON_GROUND]) {
-			this.spriteAnim = this.idleJumpAnim;
-			if (this.keyHeld_Up) {
-				this.spriteAnim = this.FlipAnim;
-			}
-		}
-		if (this.state[ATTACKING] && !this.state[ON_GROUND]) {
-			this.spriteAnim = this.highKickAnim;
-			// this.attackAnimArr[Math.floor(Math.random()*this.attackAnimArr.length)];
-		}
-		//TODO: Once crouch animation complete. Call a function to draw in fixed state instead of animation. 
-		//final drawing of sprite.
-		if (this.spriteAnim != null) {
-			//TODO :Change this.frameRow and used it for animating consilated spritesheet of player character
-			this.spriteAnim.draw(this.spriteAnim.frameIndex, this.frameRow, this.pos.x, this.pos.y, this.ang, this.state.isMovingLeft);
-			// console.log(this.spriteAnim.frameIndex);
-		}
-	}
 }
 
+//sets all values of state object to false
+playerClass.prototype.setStateToFalse = function () {
+  for (key in this.state) {
+    this.state[key] = false;
+  }
+};
+
+//sets value of given key pf state object to value passed
+playerClass.prototype.setStateValueTo = function (key, val) {
+  if (this.state.hasOwnProperty(key)) {
+    // console.log("Setting" + key + ":" + val);
+    this.state[key] = val;
+  }
+};
+
+playerClass.prototype.setupInput = function (upKey, rightKey, downKey, leftKey, attackKey, jumpKey, defendKey) {
+  this.controlKeyUp = upKey;
+  this.controlKeyRight = rightKey;
+  this.controlKeyDown = downKey;
+  this.controlKeyLeft = leftKey;
+  this.controlKeyAttack = attackKey;
+  this.controlKeyJump = jumpKey;
+  this.controlKeyDefend = defendKey;
+}
+
+ playerClass.prototype.takeDamage = function (howMuch) {
+  console.log("Damage received: " + howMuch);
+  if (this.health > 0 && !this.state[HURT]) {
+    this.health -= howMuch;
+    this.state[HURT] = true;
+    playerHitEffect(this.pos.x, this.pos.y);
+  }
+  if (this.health <= 0) {
+    console.log("PLAYER HAS 0 HP - todo: gameover/respawn");
+    this.state[DEAD] = true;
+    playerDeathEffect(this.pos.x, this.pos.y);
+    setTimeout(this.resetGame.bind(this), 1000);
+  }
+  this.resetHurtTimeout = setTimeout(this.resetHurtAnimation.bind(this), 500);
+}
+
+ playerClass.prototype.resetHurtAnimation = function () {
+  this.state[HURT] = false;
+}
+
+ playerClass.prototype.resetGame = function () {
+  loadLevel(levelOne)
+}
+
+ playerClass.prototype.init = function (whichImage, playerName) {
+  this.name = playerName;
+  this.playerPic = whichImage;
+  this.health = PLAYER_HEALTH;
+  this.doubleJumpCount = 0;
+  this.attackPower = PLAYER_ATTACK_POWER;
+  this.state = {
+    'isOnGround': true,
+    'isIdle': true,
+    'isInMotion': false,
+    'isMovingLeft': false, //Required to set character flip.
+    'isCrouching': false,
+    'isFacingUp': false, //Might be redundant
+    'isAttacking': false, //combo punches, kick on 3 continuos punch
+    'isDefending': false,
+    'isAnimating': false, // Used to set state between animation and final.
+    'isHurt': false,
+    'isDead': false,
+    'isFlying': false
+  };
+  this.setEntityPosition(WORLD_PLAYERSTART);
+} // end of playerReset func
+
+
+
+
+ playerClass.prototype.setEntityPosition = function(entityWorldIndex){
+  for (var eachRow = 0; eachRow < WORLD_ROWS; eachRow++) {
+    for (var eachCol = 0; eachCol < WORLD_COLS; eachCol++) {
+      var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
+      if (worldGrid[arrayIndex] == entityWorldIndex) {
+        worldGrid[arrayIndex] = WORLD_BACKGROUND;
+        // this.ang = -Math.PI/2;
+        this.pos.x = eachCol * WORLD_W + WORLD_W / 2;
+        this.pos.y = eachRow * WORLD_H + WORLD_H / 2;
+        return;
+      } // end of player start if
+    } // end of col for
+  } // end of row for
+  console.log("NO" + this.name + "START FOUND!");
+}
+
+ playerClass.prototype.move = function () {
+  this.boundingBox.width = this.width / 3;
+  this.boundingBox.height = this.height;
+  this.boundingBox.x = this.pos.x - this.boundingBox.width / 2;
+  this.boundingBox.y = this.pos.y - this.boundingBox.height / 2;
+
+  if (this.state[ON_GROUND]) {
+    // this.speed.x *= GROUND_FRICTION;
+    this.speed.setX(this.speed.x * GROUND_FRICTION);
+    this.doubleJumpCount = 0;
+  }
+  else { 
+    // in the air
+    this.speed.setX(this.speed.x * AIR_RESISTANCE);
+    this.speed.setY(this.speed.y + GRAVITY);
+    // improve this
+    if (this.speed.y > this.boundingBox.height / 2 + PLAYER_COLLISION_PADDING) {
+      this.speed.y = this.boundingBox.height / 2 + PLAYER_COLLISION_PADDING;
+    }
+  }
+  //Left movement
+  if (this.keyHeld_Left) {
+    //this.setStateToFalse(); //setting every value of object to false; // might be buggy
+    this.setStateValueTo(IN_MOTION, true);
+    this.setStateValueTo(MOVING_LEFT, true);
+    this.speed.setX(-PLAYER_RUN_SPEED);
+  }
+  //Right movement
+  else if (this.keyHeld_Right) {
+    //this.setStateToFalse();
+    this.setStateValueTo(IN_MOTION, true);
+    this.setStateValueTo(MOVING_LEFT, false);
+    this.speed.setX(PLAYER_RUN_SPEED);
+    // this.speed.x = PLAYER_RUN_SPEED;
+  }
+  else {
+    this.setStateValueTo(IN_MOTION, false);
+    this.setStateValueTo(IDLE, true);
+  }
+  //For crouched movement 
+  if (this.keyHeld_Down) {
+    //Up for uppercut
+    this.setStateValueTo(IDLE, false);
+    this.setStateValueTo(CROUCHING, true);
+    this.boundingBox.height = this.height / 2;
+    this.boundingBox.y = this.pos.y - this.boundingBox.height / 2;
+  }
+  else {
+    //Down for spin kick
+    this.setStateValueTo(CROUCHING, false);
+    this.setStateValueTo(IDLE, true);
+  }
+
+  if (this.keyHeld_Attack) {
+    this.setStateValueTo(IDLE, false);
+    this.setStateValueTo(IN_MOTION, false);
+    this.setStateValueTo(ATTACKING, true);
+    // if(utils.distance(player.pos,this.pos) < 60){
+    //           player.takeDamage(1);
+    //       }
+    punchSound.play();
+    this.boundingBox.width = this.width / 1.5;
+    this.boundingBox.x = this.pos.x - this.boundingBox.width / 2;
+
+    if (this.keyHeld_Up && this.state.isCrouching) {
+      this.speed.y -= 0.05;
+      this.state.isOnGround = false;
+    }
+  }
+  else {
+    this.setStateValueTo(ATTACKING, false);
+  }
+
+  if (this.keyHeld_Jump && !this.keyHeld_Up_lastframe) {
+    // this.setStateToFalse();
+    this.keyHeld_Up_lastframe = true;
+    if (this.state['isOnGround']) { // regular jump
+      jumpSound.play();
+      this.speed.setY( this.speed.getY() - JUMP_POWER);
+      this.setStateValueTo(ON_GROUND, false);
+    }
+    else if (this.doubleJumpCount < MAX_AIR_JUMPS) { // in the air?
+      jumpSound.play();
+      this.speed.setY(0);
+      this.speed.setY( this.speed.getY() - JUMP_POWER);
+      this.doubleJumpCount++;
+      this.setStateValueTo(ON_GROUND, false);
+    } 
+    else {
+      console.log("Ignoring triple jump...");
+    }
+  }
+  // avoid multiple jumps from the same keypress
+  this.keyHeld_Up_lastframe = this.keyHeld_Jump;
+
+  //Checking if player is falling or jumping.
+  //Get bounding box border coordinates and perform the same functionality for each point
+  if (this.speed.y < 0 && 
+    (isPlatformAtPixelCoord(this.pos.x + this.boundingBox.width/2, this.pos.y - this.boundingBox.height / 2) ||
+    isPlatformAtPixelCoord(this.pos.x - this.boundingBox.width/2, this.pos.y - this.boundingBox.height / 2))) {
+      this.pos.setY((Math.floor(this.pos.y / WORLD_H)) * WORLD_H + this.boundingBox.height / 2);
+      this.speed.setY(0);
+  }
+
+  if (this.speed.y > 0 && isPlatformAtPixelCoord(this.pos.x, this.pos.y + this.boundingBox.height / 2 - PLAYER_COLLISION_PADDING * 2)) {
+    this.pos.setY((1 + Math.floor(this.pos.y / WORLD_H)) * WORLD_H - this.boundingBox.height / 2 + PLAYER_COLLISION_PADDING); 
+    this.setStateValueTo(ON_GROUND, true);
+    this.speed.setY(0);
+  }
+  //checks for air/empty space
+  else if (!isPlatformAtPixelCoord(this.pos.x, this.pos.y + this.height / 2 + PLAYER_COLLISION_PADDING * 2)) {
+    this.setStateValueTo(ON_GROUND, false);
+  }
+  //Sideways collision
+  if (this.speed.x < 0 && 
+    (isPlatformAtPixelCoord(this.pos.x - this.boundingBox.width / 2, this.pos.y + this.boundingBox.height/4) ||
+    isPlatformAtPixelCoord(this.pos.x - this.boundingBox.width / 2, this.pos.y - this.boundingBox.height/4))){
+    this.pos.x = (Math.floor(this.pos.x / WORLD_W)) * WORLD_W + this.boundingBox.width / 2;
+  }
+
+  if (this.speed.x > 0 && 
+    (isPlatformAtPixelCoord(this.pos.x + this.boundingBox.width / 2, this.pos.y + this.boundingBox.height/4) ||
+     isPlatformAtPixelCoord(this.pos.x + this.boundingBox.width / 2, this.pos.y - this.boundingBox.height/4))) {
+    this.pos.x = (1 + Math.floor(this.pos.x / WORLD_W)) * WORLD_W - this.boundingBox.width / 2;
+  }
+
+  this.pos.addTo(this.speed) // same as above, but for vertical
+  playerWorldHandling(this);
+
+  if (this.spriteAnim != null) {
+    this.spriteAnim.update();
+  }
+}
+
+ playerClass.prototype.draw = function () { 
+  //TODO : Jump, Attack animation should work Only once
+  //TODO : Reset punch animation loop 
+  if (this.state[IDLE]) {
+    this.spriteAnim = this.idleAnim;
+  }
+
+  if (this.state[IN_MOTION]) {
+    this.spriteAnim = this.walkAnim;
+  }
+
+  if (this.state[HURT]) {
+    this.spriteAnim = this.hurtAnim;
+  }
+
+  if (this.state[DEAD]) {
+    this.spriteAnim = this.deadAnim;
+  }
+
+  if (this.state[CROUCHING]) {
+    this.spriteAnim = this.crouchAnim;
+    if (this.state.isInMotion) {
+      this.spriteAnim = this.rollAnim;
+    }
+  }
+  
+  if (this.state[ATTACKING]) {
+    this.spriteAnim = this.punchAnim;
+    if (this.state.isCrouching) {
+      if (this.keyHeld_Up) {
+        this.spriteAnim = this.uppercutAnim;
+      }
+      else {
+        this.spriteAnim = this.crouchedKickAnim;
+      }
+    }
+  }
+
+  //is Jumping or falling
+  if (!this.state[ON_GROUND]) {
+    this.spriteAnim = this.idleJumpAnim;
+    if (this.keyHeld_Up) {
+      this.spriteAnim = this.FlipAnim;
+    }
+  }
+
+  if (this.state[ATTACKING] && !this.state[ON_GROUND]) {
+    this.spriteAnim = this.highKickAnim;
+    // this.attackAnimArr[Math.floor(Math.random()*this.attackAnimArr.length)];
+  }
+
+  //TODO: Once crouch animation complete. Call a function to draw in fixed state instead of animation. 
+  //final drawing of sprite.
+  if (this.spriteAnim != null) {
+    //TODO :Change this.frameRow and used it for animating consilated spritesheet of player character
+    this.spriteAnim.draw(this.spriteAnim.frameIndex, this.frameRow, this.pos.x, this.pos.y, this.ang, this.state.isMovingLeft);
+    // console.log(this.spriteAnim.frameIndex);
+  }
+}
