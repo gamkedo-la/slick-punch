230ddd036b36e87c2e5b780ebb5e4ff1a5dbd75a
techieVignesh <r.vignesh149@gmail.com>
2018-08-09 18:47:40 +0530

    Object class now keeps track of object name

--- src/js/object_class.js
+++ src/js/object_class.js
@@ -1,4 +1,4 @@
-
+//TODO: change name to itemClass and change all reference
 function ObjectClass(posX, posY, width, height, tileKindHere){
 	
 	var tileKindHere = tileKindHere;
@@ -8,7 +8,60 @@ function ObjectClass(posX, posY, width, height, tileKindHere){
 		x: posX,
 		y: posY
 	};
- 
+
+	this.setName = function(){
+		if(tileKindHere == SLIME_PIT_LEFT_TOP ||
+		   tileKindHere == SLIME_PIT_MIDDLE_TOP ||
+		   tileKindHere == SLIME_PIT_RIGHT_TOP ||
+		   tileKindHere == SLIME_PIT_LEFT_TOP_ANIM ||
+		   tileKindHere == SLIME_PIT_MIDDLE_TOP_ANIM ||
+		   tileKindHere == SLIME_PIT_RIGHT_TOP_ANIM ||
+		   tileKindHere == SLIME_PIT_LEFT_CENTER ||
+		   tileKindHere == SLIME_PIT_MIDDLE_CENTER ||
+		   tileKindHere == SLIME_PIT_RIGHT_CENTER ||
+		   tileKindHere == SLIME_PIT_LEFT_BOTTOM ||
+		   tileKindHere == SLIME_PIT_MIDDLE_BOTTOM ||
+		   tileKindHere == SLIME_PIT_LEFT_TOP ||
+		   tileKindHere == SLIME_PIT_RIGHT_BOTTOM){
+		   return "Slime";
+		}
+		//use as lava
+		if(tileKindHere == RED_TILE ){
+			return "Lava";
+		}
+
+		if(tileKindHere == GREEN_VINE_WEBS){
+			return "Webs";
+		}
+
+		if(tileKindHere == THORNS){
+			return "Thorns";
+		}
+
+		if(tileKindHere == VINES_POISONOUS){
+			return "Thorns";
+		}
+
+		if(tileKindHere == PICKUP){
+			return "Diamond";
+		}
+
+		if(tileKindHere == KEY_RED){
+			return "Red Kye";
+
+		}
+
+		if(tileKindHere == KEY_BLUE){
+			return "Blue key";
+		}
+
+		if(tileKindHere == KEY_GREEN){
+			return "Green key";
+		}
+	}
+
+ 	this.name = this.setName();
+
 	this.returnEffect = function(){
 		if(tileKindHere == SLIME_PIT_LEFT_TOP ||
 		   tileKindHere == SLIME_PIT_MIDDLE_TOP ||
@@ -24,22 +77,18 @@ function ObjectClass(posX, posY, width, height, tileKindHere){
 		   tileKindHere == SLIME_PIT_LEFT_TOP ||
 		   tileKindHere == SLIME_PIT_RIGHT_BOTTOM){
 			console.log("You fell into slime");
-			this.boundingBox.name = "Slime";
 		}
 		//use as lava
 		if(tileKindHere == RED_TILE ){
 			console.log("You fell into lava");
-			this.boundingBox.name = "Lava";
 		}
 
 		if(tileKindHere == GREEN_VINE_WEBS){
 		 	console.log("You fell into webs");
-			this.boundingBox.name = "Webs";
 		}
 
 		if(tileKindHere == THORNS){
 		 	console.log("You got attacked by thorns");
-			this.boundingBox.name = "Thorns";
 		}
 
 		if(tileKindHere == VINES_POISONOUS){
@@ -48,26 +97,18 @@ function ObjectClass(posX, posY, width, height, tileKindHere){
 
 		if(tileKindHere == PICKUP){
 		 	console.log("You got picked up health");
-			this.boundingBox.name = "Diamond";
-
 		}
 
 		if(tileKindHere == KEY_RED){
 		 	console.log("You picked up a red key");
-			this.boundingBox.name = "Red Kye";
-
 		}
 
 		if(tileKindHere == KEY_BLUE){
 		 	console.log("You picked up a blue key");
-			this.boundingBox.name = "Blue key";
-
 		}
 
 		if(tileKindHere == KEY_GREEN){
 		 	console.log("You picked up green key");
-			this.boundingBox.name = "Green key";
-
 		}		
 	}
 }
