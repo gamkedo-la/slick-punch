23d43d90a5b316a805601d319e549612d1c002b6
techieVignesh <r.vignesh149@gmail.com>
2018-08-22 02:45:12 +0530

    Key collected are diplayed on UI

--- src/js/UI.js
+++ src/js/UI.js
@@ -6,13 +6,40 @@ function drawUI() {
 	//colorText(`Health : ${player.health}`, 30, 60, "yellow", "30px Tahoma");
 
 	drawHealthDisplay();
+  drawKeyDisplay();
 	drawTimer();
 }
 
+var keysPositionXInDisplay = 280;
+var keysPositionYInDisplay = 30;
+var keyHeight = keyWidth = 35;
+
+function drawKeyDisplay(){
+  if(player.key_blue){
+    canvasContext.drawImage(blueKeyAnimation,
+                            0, 0,
+                            keyWidth, keyHeight,
+                            keysPositionXInDisplay - keyWidth/2, 30 - keyHeight/2,
+                            keyHeight, keyHeight);
+  }
+  if(player.key_red){
+    canvasContext.drawImage(redKeyAnimation,
+                            0, 0,
+                            keyWidth, keyHeight,
+                            keysPositionXInDisplay + 30 - keyWidth/2, 30 - keyHeight/2,
+                            keyHeight, keyHeight);
+  }
+  if(player.key_green){
+    canvasContext.drawImage(greenKeyAnimation,
+                            0, 0,
+                            keyWidth, keyHeight,
+                            keysPositionXInDisplay + 60 - keyWidth/2, 30 - keyHeight/2,
+                            keyHeight, keyHeight);
+  }
+}
+
 function drawHealthDisplay() {
-
 	for (var thisHeart = 0; thisHeart < healthDisplay.length; thisHeart++) {
-
 		var heartImagePos = thisHeart + 1;
 		var heartImagePosFromEnd = (healthDisplay.length - 1) - thisHeart;
 
@@ -24,7 +51,6 @@ function drawHealthDisplay() {
 		} else {
 			healthDisplay[thisHeart] = 0;
 		} //end if else
-
 		//draw heart container images from left to right
 		switch (healthDisplay[thisHeart]) {
 			case 0:
@@ -37,9 +63,7 @@ function drawHealthDisplay() {
 				drawHeartImage(heartFull, heartImagePosFromEnd);
 				break;
 		} // end switch
-
 	} //end for loop
-
 } //end func drawHealthDisplay();
 
 function drawHeartImage(heartImage, posFromEnd) {
@@ -47,23 +71,18 @@ function drawHeartImage(heartImage, posFromEnd) {
 }
 
 function drawTimer() {
-
 	frameCount++
 	if (frameCount == FRAMES_PER_SECOND) {
 		timeRemaining--
-
 		if (timeRemaining == 0) {
 			player.state.isDead = true;
 			// player.takeDamage(this.attackPower);
 			setTimeout(player.resetGame.bind(player), 2000);
 		}
-
 		if(timeRemaining<0) {
 			timeRemaining = 0; // blocks showing negative
 		}
 		frameCount = 0;
 	} //end if
-
 	colorText(timeRemaining, canvas.width / 2, 40, "yellow", "30px Tahoma");
-	 
 } // end func drawTimer()
